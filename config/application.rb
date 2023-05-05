8ca0ef25ce1903d385e2cba9e15b643d4f832573
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-05 16:32:00 +0900

    feat: scaffold Anime

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module Spoilers
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.jbuilder false
+      g.system_tests = nil
+    end
   end
 end
