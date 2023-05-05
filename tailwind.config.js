d514883bda4b22bc47ed8d10017fa3a7f5077ae8
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-05 16:56:26 +0900

    chore: daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  daisyui: {
+    themes: ["light"]
+  }
 }
