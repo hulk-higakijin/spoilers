b9d0bd02ca435d95710254228ff0f484b7386c84
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-07 00:54:53 +0900

    refactor: users#show

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def display_discussions?
+    params[:display] == 'thread' || params[:display].nil?
+  end
+
+  def display_comments?
+    params[:display] == 'comment'
+  end
 end
