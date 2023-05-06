c93babba94b18aacbdac211be2f39569ba73b6e1
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-06 21:32:26 +0900

    feat: generate UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users
+  resources :users, only: [:show]
   resources :animes do
     resources :discussions, only: [:show] do
       resources :comments, only: [:create]
