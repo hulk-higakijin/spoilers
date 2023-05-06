2da2e6fec7cedae9f5430271b8aa5ad3941ca2ea
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-07 00:10:01 +0900

    feat: comment in users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show]
 
   def show
-    @discussions = @user.discussions.page(params[:page])
+    @discussions = @user.discussions.order(created_at: :desc).page(params[:page])
+    @comments = @user.comments.order(created_at: :desc).page(params[:page])
   end
 
   private
