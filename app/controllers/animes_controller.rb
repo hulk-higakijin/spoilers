23bbd36fcaf59190e43a805282da136e0834bfe2
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-06 20:49:04 +0900

    style: animes#index & delete actions in AnimesController

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,52 +1,11 @@
 class AnimesController < ApplicationController
-  before_action :set_anime, only: %i[show edit update destroy]
+  before_action :set_anime, only: %i[show]
 
   def index
     @animes = Anime.all
   end
 
   def show; end
-
-  def new
-    @anime = Anime.new
-  end
-
-  def edit; end
-
-  def create
-    @anime = Anime.new(anime_params)
-
-    respond_to do |format|
-      if @anime.save
-        format.html { redirect_to anime_url(@anime) }
-        format.json { render :show, status: :created, location: @anime }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @anime.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @anime.update(anime_params)
-        format.html { redirect_to anime_url(@anime) }
-        format.json { render :show, status: :ok, location: @anime }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @anime.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @anime.destroy
-
-    respond_to do |format|
-      format.html { redirect_to animes_url }
-      format.json { head :no_content }
-    end
-  end
 
   private
 
