2ee9a4a558c7a3a5546dad8f9393e4d62c7b6e57
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-07 00:25:25 +0900

    feat: users#edit(1)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,26 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: %i[show]
+  before_action :set_user, only: %i[show edit update]
+  before_action :need_permission, only: %i[edit update]
 
   def show
     @discussions = @user.discussions.order(created_at: :desc).page(params[:page])
     @comments = @user.comments.order(created_at: :desc).page(params[:page])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
 
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def need_permission
+      redirect_to user_path(@user) unless current_user.id == @user.id
     end
 end
