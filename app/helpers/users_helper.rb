916612c94c4401101d9fa7a6cdb6babfa8cf8db8
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-07 11:06:59 +0900

    feat: withdrawal

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,8 @@ module UsersHelper
   def display_comments?
     params[:display] == 'comment'
   end
+
+  def confirmation_message
+    '【重要】アカウントに関する全ての情報が削除されます。この操作は取り消せません。本当に削除しますか？'
+  end
 end
