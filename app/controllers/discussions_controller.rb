b3c5ff46d88aa8b6189699114470f7621093b7ba
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-07 13:31:13 +0900

    feat: create discussion

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -8,6 +8,20 @@ class DiscussionsController < ApplicationController
     @comment = current_user.comments.new(discussion: @discussion)
   end
 
+  def new
+    @discussion = current_user.discussions.new
+  end
+
+  def create
+    @discussion = current_user.discussions.new(discussion_params)
+
+    if @discussion.save
+      redirect_to anime_discussion_path(@anime, @discussion)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
     def set_anime
@@ -16,5 +30,9 @@ class DiscussionsController < ApplicationController
 
     def set_discussion
       @discussion = @anime.discussions.includes(comments: :user).find(params[:id])
+    end
+
+    def discussion_params
+      params.require(:discussion).permit(:title).merge(anime_id: @anime.id)
     end
 end
