25f4c3763a9e0ef3b1bc5c810598eae7c0734d91
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-07 12:13:18 +0900

    feat: display withdrawal status in chat

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,24 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { in: 3..15 }
   validates :email, presence: true, uniqueness: true
 
-  def display_avatar
-    avatar.attached? ? avatar : default_image
+  def display_name
+    paranoia_destroyed? ? '削除済みユーザー' : name
   end
 
-  def default_image
-    'https://pbs.twimg.com/profile_images/1639996872071974913/PPhYEW-B_400x400.jpg'
+  def display_avatar
+    return withdrawn_image if paranoia_destroyed?
+    return avatar if avatar.attached?
+
+    default_image
   end
+
+  private
+
+    def withdrawn_image
+      'https://jsbs2012.jp/wp-content/uploads/pairs_taikaisya.jpg'
+    end
+
+    def default_image
+      'https://pbs.twimg.com/profile_images/1639996872071974913/PPhYEW-B_400x400.jpg'
+    end
 end
