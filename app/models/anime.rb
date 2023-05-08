7fdfdfa92db7ef203270499a932e131ce8b5ebd0
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-08 13:22:20 +0900

    refactor: image_url in Anime.rb

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -16,14 +16,15 @@ class Anime < ApplicationRecord
   enum season: { spring: 0, summer: 1, fall: 2, winter: 3 }
 
   def display_image_url
-    if image_url.blank? || image_url == '-'
-      default_image_url
-    else
-      image_url
-    end
+    image_url.presence || default_image_url
   end
 
   def default_image_url
-    'https://pbs.twimg.com/profile_images/1639996872071974913/PPhYEW-B_400x400.jpg'
+    'https://pbs.twimg.com/profile_images/1639996872071974913/PPhYEW-B_400x400.jpg' # TODO: 画像を用意する
+  end
+
+  def image_url_from
+    return 'unknown' if image_url.blank?
+    return 'Amazon Prime Video' if image_url.include? 'amazon'
   end
 end
