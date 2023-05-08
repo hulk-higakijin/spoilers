39e5eee305a1a4b9b9ba783eff694403c4e791fc
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-08 13:17:30 +0900

    feat: import animes with seed_fu

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -9,7 +9,21 @@ class Anime < ApplicationRecord
   validates :sex, presence: true
   validates :year, presence: true, numericality: { in: 1900..2100 }
   validates :season, presence: true
+  validates :image_url, format: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/
+  validates :web_url, format: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/
 
-  enum sex: { man: 0, woman: 1, other: 2 }
+  enum sex: { men: 0, women: 1, other: 2 }
   enum season: { spring: 0, summer: 1, fall: 2, winter: 3 }
+
+  def display_image_url
+    if image_url.blank? || image_url == '-'
+      default_image_url
+    else
+      image_url
+    end
+  end
+
+  def default_image_url
+    'https://pbs.twimg.com/profile_images/1639996872071974913/PPhYEW-B_400x400.jpg'
+  end
 end
--- db/fixtures/development/001_user.rb
+++ db/fixtures/development/001_user.rb
@@ -1,5 +1,35 @@
 require 'csv'
 
-CSV.foreach('db/fixtures/csv/anime_list.csv', headers: true) do |row|
-  p row['Title']
+batch_size = 1700
+
+def hyphen_check(word)
+  word.strip == "-" ? nil : word
 end
+
+def zero_to_one(sequel)
+  sequel.to_i == 0 ? 1 : sequel.to_i
+end
+
+CSV.foreach('db/fixtures/csv/anime_list.csv', headers: true).each_slice(batch_size) do |csv_batch|
+  records = csv_batch.map do |row|
+    {
+      title: row["Title"],
+      image_url: hyphen_check(row["Thumbnail url"]),
+      title_first: hyphen_check(row["Title short1"]),
+      title_second: hyphen_check(row["Title short2"]),
+      title_third: hyphen_check(row["Title short3"]),
+      title_en: hyphen_check(row["Title en"]),
+      web_url: hyphen_check(row["Public url"]),
+      twitter_account: row["Twitter account"],
+      twitter_hash_tag: row["Twitter hash tag"],
+      sex: row["Sex"],
+      sequel: zero_to_one(row["Sequel"]),
+      year: row["Year"].to_i,
+      season: row["Season"],
+    }
+  end
+
+  Anime.seed(records)
+end
+
+
