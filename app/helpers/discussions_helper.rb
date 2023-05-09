994616d709acfeb5ac16162dc68fcb0167e35b4f
hulk-higakijin <hm385.chejptks@gmail.com>
2023-05-09 13:59:53 +0900

    feat: update archive status

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -2,4 +2,8 @@ module DiscussionsHelper
   def is_my_comment(comment)
     current_user.id == comment.user_id
   end
+
+  def reverse_archival_status_word(discussion)
+    discussion.archived? ? '再ディスカッション' : 'アーカイブする'
+  end
 end
